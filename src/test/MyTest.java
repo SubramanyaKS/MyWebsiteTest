49ed39ce8bae77f64d3275eb4d918106d8bfa0f4
Subramanya K S <subramanyaks2205@gmail.com>
2022-06-05 07:06:02 +0530

    Added MyTest java

--- src/test/MyTest.java
+++ src/test/MyTest.java
@@ -21,27 +21,48 @@ public class MyTest {
 		options.addArguments("test-type=browsers");
 		options.addArguments("disable-infobars");
 		WebDriver driver = new ChromeDriver();
+
+		//Test 01:
 		driver.get("https://subramanyaks.github.io/"); 
 		Thread.sleep(10000);
 		System.out.println("Successfully getting the Home Page");
+
+		String Title = driver.getTitle();
+		System.out.println(Title);
+
+		//Test 02:
 		driver.manage().window().maximize();
 		System.out.println("The chrome window is maximized");
 		Thread.sleep(3000);
+
+		//Test 03:
 		driver.findElement(By.linkText("PROJECT")).click();
 		Thread.sleep(5000);
 		System.out.println("Successfully get into Project");
+
+		//Test 04:
 		WebElement element=driver.findElement(By.id("main"));
 		JavascriptExecutor jse2 = (JavascriptExecutor)driver;
 		jse2.executeScript("arguments[0].scrollIntoView()", element);
-		//Actions actions = new Actions(driver);
-		//actions.moveToElement(element).click().build().perform();
 		Thread.sleep(5000);
 		System.out.println("Successfully get into Home");
+
+		//Test 05:
 		WebElement element1=driver.findElement(By.id("contact"));
 		JavascriptExecutor jse1 = (JavascriptExecutor)driver;
 		jse1.executeScript("arguments[0].scrollIntoView()", element1);
 		System.out.println("Successfully get into Contact");
-		
+		Thread.sleep(5000);
+
+		//navigate back
+		driver.navigate().back(); 
+		Thread.sleep(5000);
+
+		//navigate forward
+		driver.navigate().forward(); 
+		Thread.sleep(5000);
+		//close
+		driver.close();
 		
 	}
 
